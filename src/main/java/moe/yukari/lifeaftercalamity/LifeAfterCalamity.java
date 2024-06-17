a09ff676b374c513c82a4b36fc016fce911f1af9
yukari2024 <yukari2024@outlook.com>
2024-06-17 15:40:00 +0000

    加入灾厄之核，灾厄之镐，末影核心，末影能量电池，石镐镐头

--- src/main/java/moe/yukari/lifeaftercalamity/LifeAfterCalamity.java
+++ src/main/java/moe/yukari/lifeaftercalamity/LifeAfterCalamity.java
@@ -1,21 +1,54 @@
 package moe.yukari.lifeaftercalamity;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import moe.yukari.lifeaftercalamity.tools.CalamityItemMaterial;
+import moe.yukari.lifeaftercalamity.tools.CalamityPickaxe;
+
 public class LifeAfterCalamity implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+
     public static final Logger LOGGER = LogManager.getLogger("lifeaftercalamity");
+
+	//注册物品 Register Items
+	public static final Item CALAMITY_CORE = new Item(Item.Settings());  //灾厄核心
+	public static final Item STONE_PICKAXE_HEAD = new Item(Item.Settings());  //石镐头
+	public static final Item ENDER_CORE = new Item(Item.Settings());  //末影核心
+	public static final Item ENDER_POWER_BATTERY = new Item(Item.Settings());  //末影能量电池
+	public static final CalamityPickaxe CALAMITY_PICKAXE= new CalamityPickaxe(new CalamityItemMaterial(), 3, -0.8f, new Item.Settings());  //灾厄之镐
+
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("[LifeAfterCalamity] 世界核心已加载！ World Core is Loaded!");
+
+		//注册物品第二部分 Register Items Part 2
+		Registry.register(Registry.ITEM, new Identifier("lifeaftercalamity", "calamity_core"), CALAMITY_CORE);
+        Registry.register(Registry.ITEM, new Identifier("lifeaftercalamity", "stone_pickaxe_head"), STONE_PICKAXE_HEAD);
+		Registry.register(Registry.ITEM, new Identifier("lifeaftercalamity", "ender_core"), ENDER_CORE);
+		Registry.register(Registry.ITEM, new Identifier("lifeaftercalamity", "ender_power_battery"), ENDER_POWER_BATTERY);
+		Registry.register(Registry.ITEM, new Identifier("lifeaftercalamity", "calamity_pickaxe"), CALAMITY_PICKAXE);
+
 	}
+
+	public static final ItemGroup CALAMITY_GROUP = FabricItemGroupBuilder.create(
+		new Identifier("lifeaftercalamity", "calamity"))
+		.icon(() -> new ItemStack(LifeAfterCalamity.CALAMITY_CORE))
+		.appendItems(stacks -> {
+			stacks.add(new ItemStack(LifeAfterCalamity.CALAMITY_CORE));
+			stacks.add(new ItemStack(LifeAfterCalamity.CALAMITY_PICKAXE));
+			stacks.add(new ItemStack(LifeAfterCalamity.STONE_PICKAXE_HEAD));
+			stacks.add(new ItemStack(LifeAfterCalamity.ENDER_CORE));
+			stacks.add(new ItemStack(LifeAfterCalamity.ENDER_POWER_BATTERY));
+		})
+		.build();
+	
 }
